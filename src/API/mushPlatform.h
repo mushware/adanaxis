26399228f85dd3bd6536795c8f6a5b1c483c6c6d
Andy Southgate <andy@mushware.com>
2002-06-06 15:07:55 +0000

    Mouse fixes

--- src/API/mushPlatform.h
+++ src/API/mushPlatform.h
@@ -1,11 +1,14 @@
 #ifndef MUSHPLATFORM_H
 #define MUSHPLATFORM_H
 /*
- * $Id$
- * $Log$
+ * $Id: mushPlatform.h,v 1.1 2002/06/06 12:30:02 southa Exp $
+ * $Log: mushPlatform.h,v $
+ * Revision 1.1  2002/06/06 12:30:02  southa
+ * Added support for src/Support/platform
+ *
  */
 
 // Input utilities
-#include "Support/PlatformInputUtils.h"
+#include "Platform/PlatformInputUtils.h"
 
 #endif
