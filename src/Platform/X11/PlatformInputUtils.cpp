61d2e782952bba2eb9b84110c54d9507fc59d0c8
Andy Southgate <andy@mushware.com>
2002-06-26 17:56:34 +0000

    Created

---
+++ src/Platform/X11/PlatformInputUtils.cpp
@@ -0,0 +1,39 @@
+/*
+ * $Id: PlatformInputUtils.cpp,v 1.1 2002/06/06 14:59:57 southa Exp $
+ * $Log: PlatformInputUtils.cpp,v $
+ * Revision 1.1  2002/06/06 14:59:57  southa
+ * Mouse fixes
+ *
+ * Revision 1.1  2002/06/06 12:30:18  southa
+ * Added support for src/Support/platform
+ *
+ * Revision 1.1  2002/06/06 10:40:20  southa
+ * Created
+ *
+ */
+
+/*********************************
+ *
+ * X11 support file
+ *
+ *********************************/
+
+#include "mushPlatform.h"
+
+
+bool
+PlatformInputUtils::MouseDeltaPrologue(S32& ioX, S32& ioY, S32& ioLastX, S32& ioLastY)
+{
+}
+
+void
+PlatformInputUtils::GetMouseDeltas(S32& outXDelta, S32& outYDelta, S32& ioX, S32& ioY, S32& ioLastX, S32& ioLastY)
+{
+    throw(LogicFail("GetMouseDeltas not implemented for X11"));
+}
+
+void
+PlatformInputUtils::MouseDeltaEpilogue(S32& ioX, S32& ioY, S32& ioLastX, S32& ioLastY)
+{
+}
+
---
+++ src/Platform/X11/PlatformMiscUtils.cpp
@@ -0,0 +1,63 @@
+/*
+ * $Id: PlatformMiscUtils.cpp,v 1.5 2002/06/20 11:06:15 southa Exp $
+ * $Log: PlatformMiscUtils.cpp,v $
+ * Revision 1.5  2002/06/20 11:06:15  southa
+ * Updated for cygwin
+ *
+ * Revision 1.4  2002/06/13 15:53:33  southa
+ * Path fixes
+ *
+ * Revision 1.3  2002/06/11 18:12:04  southa
+ * Fixed for cygwin
+ *
+ * Revision 1.2  2002/06/11 16:36:13  southa
+ * Fixes
+ *
+ * Revision 1.1  2002/06/11 16:25:00  southa
+ * New paths to data files
+ *
+ */
+
+#include "mushPlatform.h"
+
+
+string
+PlatformMiscUtils::GetApplPath(int argc, char *argv[])
+{
+    string systemPath=GetSystemPath(argc, argv);
+    string::size_type pos=string::npos;
+    pos = systemPath.rfind('/', pos-1);
+    if (pos==string::npos || pos == 0)
+    {
+        cerr << "Couldn't decode application path from '" << systemPath << "'" << endl;
+        exit(1);
+    }
+    systemPath.resize(pos);
+
+    return systemPath;
+}
+
+string
+PlatformMiscUtils::GetSystemPath(int argc, char *argv[])
+{
+    string appPath;
+    char *pBuffer=new char[MAXPATHLEN];
+    if (getcwd(pBuffer, MAXPATHLEN) &&
+        strlen(pBuffer) > 1)
+    {
+        appPath=string(pBuffer);
+    }
+    else
+    {
+        cerr << "Couldn't decode system path from '" << appPath << "'" << endl;
+        exit(1);
+    }
+    delete[] pBuffer;
+
+    return appPath;
+}
+
+void
+PlatformMiscUtils::TweakArgs(string& ioStr)
+{
+}
