394e12cf9c9e994086b0c98c6f91b5a9a48eacd9
Andy Southgate <andy@mushware.com>
2002-07-01 13:46:12 +0000

    MacOS X installer automation

--- MakeRelease.sh
+++ MakeRelease.sh
@@ -9,5 +9,11 @@ echo Making directories in $DEST
 find . -path '*CVS' -prune -o -type d -exec mkdir -p $DEST/{} \;
 echo Copying from . to $DEST
 find . -path '*CVS' -prune -o -name '._ASP500.csv' -o -name '.DS_Store' -prune -o -type f -exec cp -p {} $DEST/{} \;
+cp ../AUTHORS $DEST/AUTHORS.txt
+cp ../ChangeLog $DEST/ChangeLog.txt
+cp ../COPYING $DEST/COPYING.txt
+cp ../INSTALL $DEST/INSTALL.txt
+cp ../NEWS $DEST/NEWS.txt
+cp ../README $DEST/README.txt
 chmod -R g+w $DEST
 echo Done
