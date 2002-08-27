bdb74f131722da116df918d19513239e4fb42b75
Andy Southgate <andy@mushware.com>
2002-08-27 09:07:52 +0000

    Removed warning

--- src/CoreApp/LaunchCommandHandler.cpp
+++ src/CoreApp/LaunchCommandHandler.cpp
@@ -14,8 +14,11 @@
 
 
 /*
- * $Id: LaunchCommandHandler.cpp,v 1.10 2002/08/07 13:36:46 southa Exp $
+ * $Id: LaunchCommandHandler.cpp,v 1.11 2002/08/27 08:56:18 southa Exp $
  * $Log: LaunchCommandHandler.cpp,v $
+ * Revision 1.11  2002/08/27 08:56:18  southa
+ * Source conditioning
+ *
  * Revision 1.10  2002/08/07 13:36:46  southa
  * Conditioned source
  *
@@ -89,6 +92,16 @@ LaunchCommandHandler::Launch(CoreCommand& ioCommand, CoreEnv &ioEnv)
         for (tSize i=0; i<funcName.size(); i++)
         {
             if (inQuotes != '\0')
+*
+* (Mushware file header version 1.0)
+*
+* This file contains original work by Andy Southgate.  Contact details can be
+* found at http://www.mushware.co.uk.  This file was placed in the Public
+* Domain by Andy Southgate and Mushware Limited in 2002.
+*
+* This software carries NO WARRANTY of any kind.
+*
+****************************************************************************/
             {
                 if (funcName[i] == inQuotes)
                 {
@@ -146,7 +159,7 @@ LaunchCommandHandler::Launch(CoreCommand& ioCommand, CoreEnv &ioEnv)
     else
     {
         close(p[kStdout]);
-        CoreApp::Instance().AddChild(cpid, p[kStdin], NULL);
+        CoreApp::Instance().AddChild(cpid, p[kStdin], 0);
     }
     return CoreScalar(0);
 #endif
