28bc1e91788a8d1e88aeb45e0c16a05646a1d517
Andy Southgate <andy@mushware.com>
2002-08-27 09:09:06 +0000

    Fixed file corruption

--- src/CoreApp/LaunchCommandHandler.cpp
+++ src/CoreApp/LaunchCommandHandler.cpp
@@ -92,16 +92,6 @@ LaunchCommandHandler::Launch(CoreCommand& ioCommand, CoreEnv &ioEnv)
         for (tSize i=0; i<funcName.size(); i++)
         {
             if (inQuotes != '\0')
-*
-* (Mushware file header version 1.0)
-*
-* This file contains original work by Andy Southgate.  Contact details can be
-* found at http://www.mushware.co.uk.  This file was placed in the Public
-* Domain by Andy Southgate and Mushware Limited in 2002.
-*
-* This software carries NO WARRANTY of any kind.
-*
-****************************************************************************/
             {
                 if (funcName[i] == inQuotes)
                 {
