6077265922e6794aae57f5464c039a3d194ea3f1
Andy Southgate <andy@mushware.com>
2003-09-21 09:56:39 +0000

    Re-added

---
+++ src/Mushcore/MushcoreXMLOStream.cpp
@@ -0,0 +1,41 @@
+//%Header {
+/*****************************************************************************
+ *
+ * File: src/Mushcore/MushcoreXMLOstream.cpp
+ *
+ * This file contains original work by Andy Southgate.  Contact details can be
+ * found at http://www.mushware.com/.  This file was placed in the Public
+ * Domain by Andy Southgate and Mushware Limited in 2002-2003.
+ *
+ * This software carries NO WARRANTY of any kind.
+ *
+ ****************************************************************************/
+//%Header } nJdVJ8jiSMikKV4dE2rJkw
+/*
+ * $Id: MushcoreXMLOstream.cpp,v 1.1 2003/09/21 09:51:09 southa Exp $
+ * $Log: MushcoreXMLOstream.cpp,v $
+ * Revision 1.1  2003/09/21 09:51:09  southa
+ * Stream autogenerators
+ *
+ */
+
+#include "MushcoreXMLOstream.h"
+
+#include "MushcoreSTL.h"
+MushcoreXMLOStream::MushcoreXMLOStream()
+{
+    m_pStream = new std::ostringstream;
+}
+
+MushcoreXMLOStream::~MushcoreXMLOStream()
+{
+    delete m_pStream;
+}
+
+void
+MushcoreXMLOStream::Print(std::ostream& ioOut) const
+{
+    ioOut << "[";
+    ioOut << "*m_pStream=" << dynamic_cast<std::ostringstream *>(m_pStream)->str();
+    ioOut << "]";
+}
