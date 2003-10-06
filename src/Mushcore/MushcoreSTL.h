9d31795869d61c21fb592215a3dbd2efb4010feb
Andy Southgate <andy@mushware.com>
2003-10-06 22:50:02 +0000

    Added M_PI

--- src/Mushcore/MushcoreSTL.h
+++ src/Mushcore/MushcoreSTL.h
@@ -16,8 +16,11 @@
  ****************************************************************************/
 //%Header } R50LoW9d2MyCM0eN10Ob1A
 /*
- * $Id: MushcoreSTL.h,v 1.3 2003/08/21 23:09:16 southa Exp $
+ * $Id: MushcoreSTL.h,v 1.4 2003/09/17 19:40:36 southa Exp $
  * $Log: MushcoreSTL.h,v $
+ * Revision 1.4  2003/09/17 19:40:36  southa
+ * Source conditioning upgrades
+ *
  * Revision 1.3  2003/08/21 23:09:16  southa
  * Fixed file headers
  *
@@ -49,6 +52,10 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 //%includeGuardEnd {
 #endif
