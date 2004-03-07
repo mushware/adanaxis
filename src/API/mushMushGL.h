06b0ce20b2b5ef5248c1604e337b55bc4fa7a2bf
Andy Southgate <andy@mushware.com>
2004-03-07 12:06:22 +0000

    Rendering work

---
+++ src/API/mushMushGL.h
@@ -0,0 +1,33 @@
+//%includeGuardStart {
+#ifndef MUSHMUSHGL_H
+#define MUSHMUSHGL_H
+//%includeGuardStart } 73409qbWKt4XwsEIBqVdyg
+//%Header {
+/*****************************************************************************
+ *
+ * File: src/API/mushMushGL.h
+ *
+ * This file contains original work by Andy Southgate.  Contact details can be
+ * found at http://www.mushware.com/.  This file was placed in the Public
+ * Domain by Andy Southgate and Mushware Limited in 2002-2004.
+ *
+ * This software carries NO WARRANTY of any kind.
+ *
+ ****************************************************************************/
+//%Header } YM/gPJAycgAhRDqOSUf1uQ
+/*
+ * $Id: mushMushGL.h,v 1.1 2004/01/06 20:46:48 southa Exp $
+ * $Log: mushMushGL.h,v $
+ */
+
+#if defined(HAVE_MUSHGL_MUSHGL_H)
+#include <MushGL/MushGL.h>
+#elif defined(HAVE_MUSHGL_H)
+#include <MushGL.h>
+#else
+#include "MushGL/MushGL.h"
+#endif
+
+//%includeGuardEnd {
+#endif
+//%includeGuardEnd } hNb4yLSsimk5RFvFdUzHEw
