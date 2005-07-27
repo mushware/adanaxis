7a00bee00beb60ea991cfa72e18feab9b5cd81e6
Andy Southgate <andy@mushware.com>
2005-07-27 18:08:44 +0000

    Collision checking

---
+++ src/API/mushMushCollision.h
@@ -0,0 +1,44 @@
+//%includeGuardStart {
+#ifndef MUSHMUSHCOLLISION_H
+#define MUSHMUSHCOLLISION_H
+//%includeGuardStart } GU8mG5zb7tu8lOWXkuIlTw
+//%Header {
+/*****************************************************************************
+ *
+ * File: src/API/mushMushCollision.h
+ *
+ * Author: Andy Southgate 2002-2005
+ *
+ * This file contains original work by Andy Southgate.  The author and his
+ * employer (Mushware Limited) irrevocably waive all of their copyright rights
+ * vested in this particular version of this file to the furthest extent
+ * permitted.  The author and Mushware Limited also irrevocably waive any and
+ * all of their intellectual property rights arising from said file and its
+ * creation that would otherwise restrict the rights of any party to use and/or
+ * distribute the use of, the techniques and methods used herein.  A written
+ * waiver can be obtained via http://www.mushware.com/.
+ *
+ * This software carries NO WARRANTY of any kind.
+ *
+ ****************************************************************************/
+//%Header } LtvaZieEkuk6NEFsGO8x0Q
+/*
+ * $Id$
+ * $Log$
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#if defined(HAVE_MUSHCOLLISION_MUSHCOLLISION_H)
+#include <MushCollision/MushCollision.h>
+#elif defined(HAVE_MUSHCOLLISION_H)
+#include <MushCollision.h>
+#else
+#include "MushCollision/MushCollision.h"
+#endif
+
+//%includeGuardEnd {
+#endif
+//%includeGuardEnd } hNb4yLSsimk5RFvFdUzHEw
