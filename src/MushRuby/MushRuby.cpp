6f3e371545963e7e3008d19fedf4eb20e8216e24
Andy Southgate <andy@mushware.com>
2006-04-19 20:21:34 +0000

    Added Ruby framework

---
+++ src/MushRuby/MushRuby.cpp
@@ -0,0 +1,26 @@
+//%Header {
+/*****************************************************************************
+ *
+ * File: src/MushRuby/MushRuby.cpp
+ *
+ * Author: Andy Southgate 2002-2006
+ *
+ * This file contains original work by Andy Southgate.  The author and his
+ * employer (Mushware Limited) irrevocably waive all of their copyright rights
+ * vested in this particular version of this file to the furthest extent
+ * permitted.  The author and Mushware Limited also irrevocably waive any and
+ * all of their intellectual property rights arising from said file and its
+ * creation that would otherwise restrict the rights of any party to use and/or
+ * distribute the use of, the techniques and methods used herein.  A written
+ * waiver can be obtained via http://www.mushware.com/.
+ *
+ * This software carries NO WARRANTY of any kind.
+ *
+ ****************************************************************************/
+//%Header } 8GiZpL69FVNYsmGUBy94qA
+/*
+ * $Id$
+ * $Log$
+ */
+
+#include "MushRuby.h"
