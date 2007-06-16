fd5b235d5b9aeff7fe6014235e87509815b3664b
cvs-fast-export <cvs-fast-export>
2007-06-16 11:09:32 +0000

    Synthetic commit for tag ADANAXIS_1_0_2_RELEASE_MACOSX

--- win32/SignExe.rb
+++ win32/SignExe.rb
@@ -13,20 +13,21 @@
 keyPrefix="J:\\mushware"
 toolPath='C:/Program Files/Microsoft Visual Studio .NET 2003/Common7/Tools/Bin'
 
-fileList = Dir.glob("../VisualC/project/Release/*.exe")
-fileList += Dir.glob("*.exe")
+fileList = []
+fileList << Dir.glob("../VisualC/project/Release/*.exe")
+fileList << Dir.glob("*.exe")
 
 fileList.each do |filename|
   puts "Sign #{filename}?"
-  if STDIN.gets[0] == 121
+  if STDIN.getc == 121
     system("\"#{toolPath}/SignCode.exe\""+
          " -spc \"#{keyPrefix}cert.spc\" -v \"#{keyPrefix}key.pvk\" -i \"http://www.mushware.com/\""+
          " -n \"Mushware installer/exe\" -t \"http://timestamp.verisign.com/scripts/timstamp.dll\""+
-         " -a sha1 \"#{filename}\""
+         " \"#{filename}\""
           ) or raise "Failed"
   end        
-  system("\"#{toolPath}/ChkTrust.exe\" -q \"#{filename}\""
-        ) or puts "**** ChkTrust.exe Failed"
+    system("\"#{toolPath}/ChkTrust.exe\" \"#{filename}\""
+          ) or raise "Failed"
   
 end
 
