20284d85dd71f4f34c0576838d4c0ba70ea34502
Andy Southgate <andy@mushware.com>
2007-06-16 10:31:42 +0000

    Code signing

--- win32/FileListToNSI.rb
+++ win32/FileListToNSI.rb
@@ -1,3 +1,15 @@
+##############################################################################
+#
+# This file contains original work by Andy Southgate.  Contact details can be
+# found at http://www.mushware.com.  This file was placed in the Public
+# Domain by Andy Southgate and Mushware Limited 2007.
+#
+# This software carries NO WARRANTY of any kind.
+#
+##############################################################################
+#
+# $Id: MakeInstaller.sh,v 1.10 2007/06/16 08:26:50 southa Exp $
+# $Log: MakeInstaller.sh,v $
 
 require 'FileUtils'
 require 'Find'
--- win32/MakeInstaller.sh
+++ win32/MakeInstaller.sh
@@ -2,15 +2,18 @@
 ##############################################################################
 #
 # This file contains original work by Andy Southgate.  Contact details can be
-# found at http://www.mushware.co.uk.  This file was placed in the Public
+# found at http://www.mushware.com.  This file was placed in the Public
 # Domain by Andy Southgate and Mushware Limited 2002-2006.
 #
 # This software carries NO WARRANTY of any kind.
 #
 ##############################################################################
 #
-# $Id: MakeInstaller.sh,v 1.9 2007/06/14 14:59:15 southa Exp $
+# $Id: MakeInstaller.sh,v 1.10 2007/06/16 08:26:50 southa Exp $
 # $Log: MakeInstaller.sh,v $
+# Revision 1.10  2007/06/16 08:26:50  southa
+# Prerelease tweaks
+#
 # Revision 1.9  2007/06/14 14:59:15  southa
 # win32 fixes
 #
