9a080ed8284cd9898f4228129e00cf198f6dda04
Andy Southgate <andy@mushware.com>
2007-06-16 10:31:33 +0000

    Code signing

--- win32/SignExe.rb
+++ win32/SignExe.rb
@@ -1,4 +1,14 @@
 #!ruby -w
+##############################################################################
+#
+# This file contains original work by Andy Southgate.  Contact details can be
+# found at http://www.mushware.com.  This file was placed in the Public
+# Domain by Andy Southgate and Mushware Limited 2007.
+#
+# This software carries NO WARRANTY of any kind.
+#
+##############################################################################
+#
 
 keyPrefix="J:\\mushware"
 toolPath='C:/Program Files/Microsoft Visual Studio .NET 2003/Common7/Tools/Bin'
