12bef6db3bb089a17a6d2c7ef92c2972da392aff
Andy Southgate <andy@mushware.com>
2007-06-30 16:02:14 +0000

    Generic packaging

--- x11/icons/MakeIcons.rb
+++ x11/icons/MakeIcons.rb
@@ -17,8 +17,11 @@
 # This software carries NO WARRANTY of any kind.
 #
 ##############################################################################
-# $Id: MakeIcons.rb,v 1.1 2007/06/29 11:03:13 southa Exp $
+# $Id: MakeIcons.rb,v 1.2 2007/06/30 11:45:47 southa Exp $
 # $Log: MakeIcons.rb,v $
+# Revision 1.2  2007/06/30 11:45:47  southa
+# X11 release
+#
 # Revision 1.1  2007/06/29 11:03:13  southa
 # Created
 #
@@ -32,6 +35,7 @@ path = File.dirname(__FILE__)
 for size in ["16","32","48"]
   File.copy(path+"/adanaxis-#{size}.png", path+"/adanaxisdemo-#{size}.png")
   File.copy(path+"/adanaxis-#{size}.png", path+"/adanaxisgpl-#{size}.png")
+  File.copy(path+"/adanaxis-#{size}.png", path+"/adanaxisstub-#{size}.png")
 end
 
 for name in ["adanaxis", "adanaxisdemo", "adanaxisgpl"]
