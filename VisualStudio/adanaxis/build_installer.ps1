1c487c0bc59116204495e12e26f7ccc2d2554cfa
Andy Southgate <andy@mushware.com>
2020-06-19 10:33:45 +0100

    Windows build and WiX installer work.

--- VisualStudio/adanaxis/build_installer.ps1
+++ VisualStudio/adanaxis/build_installer.ps1
@@ -5,24 +5,28 @@ $candle_args_pre = @()
 $candle_args_post = @()
 $light_args_post = @()
 
-Remove-Item -Force $msi_path
+Remove-Item -ErrorAction Ignore -Force $msi_path
 
-$datadirs = @("mush", "pixels", "ruby", "spaces", "system", "waves")
+$datadirs = @("mush", "mushruby", "pixels", "ruby", "spaces", "system", "waves")
 foreach ($datadir in $datadirs) {
-    $sourcedir="../../adanaxis-data/$datadir"
-    $destfile="adanaxis_$datadir.wxs"
+    if ($datadir -eq "mushruby") {
+        $sourcedir = "../../adanaxis-mushruby/$datadir"
+    } else {
+        $sourcedir = "../../adanaxis-data/$datadir"
+    }
+    $destfile="adanaxis_$datadir.wxi"
     $candle_args_pre += "-dadanaxis_$datadir=""$sourcedir"""  
     $candle_args_post += $destfile
     $obj_file = "${outpath}adanaxis_$datadir.wixobj"
     $light_args_post += $obj_file
-    Remove-Item -Force $destfile
-    Remove-Item -Force $obj_file
+    Remove-Item -ErrorAction Ignore -Force $destfile
+    Remove-Item -ErrorAction Ignore -Force $obj_file
     $heat_command="heat.exe dir $sourcedir -dr INSTALLFOLDER -cg Adanaxis_$datadir -gg -sfrag -var var.adanaxis_$datadir -v -o $destfile"
     Write-Host "Executing $heat_command`n"
     Invoke-Expression -ErrorAction Stop $heat_command
 }
 
-$candle_command = "candle.exe $($candle_args_pre -join "" "") -v -out $outpath -arch x86 ..\adanaxis-wixsetup\Adanaxis.wxs $($candle_args_post -join "" "")"
+$candle_command = "candle.exe $($candle_args_pre -join "" "") -v -out $outpath -arch x86 Adanaxis.wxs $($candle_args_post -join "" "")"
 Write-Host "Executing $candle_command`n"
 Invoke-Expression -ErrorAction Stop $candle_command
 
--- VisualStudio/adanaxis/build_travis.bat
+++ VisualStudio/adanaxis/build_travis.bat
@@ -1,14 +1,13 @@
 
-powershell.exe -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command scripts/GetWindowsDeps.ps1
-
-SET "PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin;%PATH%"
 CD "VisualStudio\adanaxis"
-MSBuild.exe adanaxis.sln
 
+powershell.exe -ExecutionPolicy Bypass -NonInteractive -NoProfile -Command ./GetWindowsDeps.ps1
+
+SET "MSBUILD_ROOT=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 SET "WIX_ROOT=C:\Program Files (x86)\WiX Toolset v3.11\bin"
 
-IF NOT EXIST "%WIX_ROOT%" powershell.exe -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command ./install_wix.ps1
+IF NOT EXIST "%WIX_ROOT%" powershell.exe -ExecutionPolicy Bypass -NonInteractive -NoProfile -Command ./install_wix.ps1
 
-SET "PATH=%WIX_ROOT%;%PATH%"
+SET "PATH=%WIX_ROOT%;%MSBUILD_ROOT%;%PATH%"
 
-powershell.exe -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command ./build_installer.ps1
+MSBuild.exe adanaxis.sln
