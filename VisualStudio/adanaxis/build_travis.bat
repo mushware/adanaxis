5848860226c92bdf78aae5725ac35ab572716ece
Andy Southgate <andy@mushware.com>
2020-06-19 23:23:23 +0100

    Fixes file copying for Release installer.

--- VisualStudio/adanaxis/build_travis.bat
+++ VisualStudio/adanaxis/build_travis.bat
@@ -1,13 +1,17 @@
 
 CD "VisualStudio\adanaxis"
 
-powershell.exe -ExecutionPolicy Bypass -NonInteractive -NoProfile -Command ./GetWindowsDeps.ps1
+powershell.exe -ExecutionPolicy Bypass -NonInteractive -NoProfile -File ./GetWindowsDeps.ps1
 
 SET "MSBUILD_ROOT=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 SET "WIX_ROOT=C:\Program Files (x86)\WiX Toolset v3.11\bin"
 
-IF NOT EXIST "%WIX_ROOT%" powershell.exe -ExecutionPolicy Bypass -NonInteractive -NoProfile -Command ./install_wix.ps1
+IF NOT EXIST "%WIX_ROOT%" powershell.exe -ExecutionPolicy Bypass -NonInteractive -NoProfile -File ./install_wix.ps1
 
 SET "PATH=%WIX_ROOT%;%MSBUILD_ROOT%;%PATH%"
 
+MSBuild.exe adanaxis.sln /t:adanaxis /p:Configuration="Debug"
 MSBuild.exe adanaxis.sln /t:adanaxis /p:Configuration="Release"
+
+START /B /WAIT "" msiexec.exe /q /i msi\Release\Adanaxis.msi
+powershell.exe -ExecutionPolicy Bypass -NonInteractive -NoProfile -Command "Uninstall-Package Adanaxis"
